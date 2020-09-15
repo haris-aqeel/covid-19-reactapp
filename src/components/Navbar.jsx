c8c11ec7ccf03867d1cbd08ce23cd95cbef39dbd
haris-aqeel <harisaqeel.2001@gmail.com>
2020-09-15 11:15:07 +0500

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+      textAlign: "left",
+      paddingLeft: "60px"
+    },
+  }),
+);
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Covid-19 Tracker App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
