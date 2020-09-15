2b10f0e9fa5a09466b2323fa3cf0ec8da4968eda
haris-aqeel <harisaqeel.2001@gmail.com>
2020-09-15 11:14:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Search from './components/Search'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Header/>
     </div>
   );
 }
