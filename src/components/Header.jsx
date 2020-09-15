f1562ac4077837422ce1472cca27e670b4b25cf0
haris-aqeel <harisaqeel.2001@gmail.com>
2020-09-15 11:14:57 +0500

    Create Header.jsx
    Added a Material UI label

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Label from '../images/covid-19-label.png' 
+
+const Header = () => {
+    return(
+        <div className='Header'>
+            <img src={Label} style={{marginTop: "30px"}} alt="covid19Title"/>
+        </div>
+
+    );
+}
+
+export default Header;
